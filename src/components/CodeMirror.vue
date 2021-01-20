b19d98978ed0e15643a1db494f6643495143da61
kyoruni <kyoruni@gmail.com>
2021-01-20 13:04:54 +0900

    :wrench: CodeMirror表示

--- src/components/CodeMirror.vue
+++ src/components/CodeMirror.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="code-mirror">
-    Code Mirror
+    <codemirror
+      v-model="code"
+      :options="cmOptions"
+    />
   </div>
 </template>
+
+<script>
+import { codemirror } from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+// import language js
+import 'codemirror/mode/javascript/javascript.js'
+// import theme style
+import 'codemirror/theme/base16-dark.css'
+
+export default {
+  components: {
+    codemirror
+  },
+  data () {
+    return {
+      code: 'const a = 10',
+      cmOptions: {
+        tabSize: 4,
+        mode: 'text/javascript',
+        theme: 'base16-dark',
+        lineNumbers: true,
+        line: true
+      }
+    }
+  }
+}
+</script>
